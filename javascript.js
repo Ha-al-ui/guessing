fa16c14d0c096592a1055b533c79538f2656f087
hamzasolicode <91888808+hamzasolicode@users.noreply.github.com>
2021-12-03 09:23:54 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -6,33 +6,52 @@ var answer = Math.floor(Math.random()*100) + 1;
 var no_of_guesses = 0;
 var guessed_nums = [];
 
-
 function play(){
-    var user_guess = document.getElementById("guess") .
-    value;
-    if(user_guess < 1 || user_guess > 100 && no_of_guesses<=10){
+    
+    
+    var user_guess = document.getElementById("guess").value;
+    if(user_guess < 1 || user_guess > 100){
         alert("Please enter a number between 1 and 100.");
     }
     else{
         guessed_nums.push(user_guess);
-        no_of_guesses+= 1;
+        no_of_guesses++;
+         
+        
 
         if(user_guess < answer && no_of_guesses<=10){
             msg1.textContent = "Your guess is too low.";
-            msg2.textContent ="No. of guesses:" + no_of_guesses;
-            msg3.textContent ="Guessed numbers are:" + guessed_nums;
+            msg2.textContent = "No. of guesses: " + no_of_guesses;
+            msg3.textContent = "Guessed numbers are: " +
+            guessed_nums;
+            
+        }
+        else if (user_guess==answer && no_of_guesses==1){
+        msg1.textContent = "!! Wow Spectacular !!"
+        msg2.textContent = "!! Bravo !!"
+        }
+     
+        
+
+        else if(user_guess > answer && no_of_guesses<=10){
+            msg1.textContent = "Your guess is too high.";
+            msg2.textContent = "No. of guesses: " + no_of_guesses;
+            msg3.textContent = "Guessed numbers are: " +
+            guessed_nums;
+        }
+        
+        else if(user_guess == answer){
+            msg1.textContent = "!! YOU Win !!";
+            msg2.textContent = "The number was: " + answer;
+            msg3.textContent = "You guessed it in "+ no_of_guesses + " guesses";
+            document.getElementById("my_btn").disabled = true;
+        }
+        else if (user_guess != answer){
+            msg1.textContent = "GAME OVER";
+            msg2.textContent = "";
+            msg3.textContent = " MAX NUMBER OF GUESSSES REACHED";
 
         }
-        else if(user_guess > answer && no_of_guesses<=10){
-            msg1.textContent = "Your guess is too hight.";
-            msg2.textContent ="No. of guesses:" + no_of_guesses;
-            msg3.textContent ="Guessed numbers are:" + guessed_nums;
-        }
-        else if(user_guess == answer && no_of_guesses<=10){
-            msg1.textContent = "Bravoo You Win !!";
-            msg2.textContent = "The number was:" + answer;
-            msg3.textContent = "You guessed it in:" + no_of_guesses + " guesses";
-            document.getElementById("my_btn") .disabled = true ;
-        }
+       
     }
 }
